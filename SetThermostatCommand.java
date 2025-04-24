c56ff9120373bb83657e3646d35c44629797a0d9
Gulnur Kadyrmanova <s23070635@narxoz.kz>
2025-04-24 18:28:28 +0500

    Create SetThermostatCommand.java

---
+++ SetThermostatCommand.java
@@ -0,0 +1,19 @@
+public class SetThermostatCommand implements Command {
+    private Thermostat thermostat;
+    private int temperature;
+
+    public SetThermostatCommand(Thermostat thermostat, int temperature) {
+        this.thermostat = thermostat;
+        this.temperature = temperature;
+    }
+
+    @Override
+    public void execute() {
+        thermostat.setTemperature(temperature);
+    }
+
+    @Override
+    public void undo() {
+        thermostat.revertTemperature();
+    }
+}
