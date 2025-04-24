c558c8e55102bb95feffa4c37d14e7ba69cf31d7
Gulnur Kadyrmanova <s23070635@narxoz.kz>
2025-04-24 15:23:08 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
   public static void main(String[] args) {
-    
+      SupportHandler faq = new FAQBotHandler();
+      SupportHandler junior = new JuniorSupportHandler();
   }
 }
