04418cf6ae22852aeae4e320a1c1a76d788fd0d4
Gulnur Kadyrmanova <s23070635@narxoz.kz>
2025-04-24 18:29:37 +0500

    Update MainCommand.java

--- MainCommand.java
+++ MainCommand.java
@@ -1,5 +1,19 @@
 public class MainCommand {
     public static void main(String[] args) {
-      
+        Light light = new Light();
+        Thermostat thermostat = new Thermostat();
+
+        Command lightCommand = new TurnOnLightCommand(light);
+        Command tempCommand = new SetThermostatCommand(thermostat, 22);
+
+        SmartHomeRemoteControl remote = new SmartHomeRemoteControl();
+        remote.setCommand("light", lightCommand);
+        remote.setCommand("thermo", tempCommand);
+
+        remote.pressButton("light");
+        remote.pressButton("thermo");
+
+        System.out.println("Undo last command");
+        remote.undoButton();
     }
 }
