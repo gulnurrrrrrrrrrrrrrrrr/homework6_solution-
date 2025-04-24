5613131d18a6fa1db51d2d9f833ad70b65e2c97a
Gulnur Kadyrmanova <s23070635@narxoz.kz>
2025-04-24 15:39:10 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,12 @@ public class Main{
   public static void main(String[] args) {
       SupportHandler faq = new FAQBotHandler();
       SupportHandler junior = new JuniorSupportHandler();
-      
+      SupportHandler senior = new SeniorSupportHandler();
+    
+        String[] issues = { "password_reset", "refund_request", "account_ban", "unknown_bug" };
 
+        for (String issue : issues) {
+            faq.handle(issue);
+        }
   }
 }
