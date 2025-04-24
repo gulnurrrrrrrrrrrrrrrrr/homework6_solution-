ed3ef964695f4c65c19e95be48254a4cf593fa9e
Gulnur Kadyrmanova <s23070635@narxoz.kz>
2025-04-24 15:18:55 +0500

    Update FAQBotHandler.java

--- FAQBotHandler.java
+++ FAQBotHandler.java
@@ -1,5 +1,11 @@
 public class FAQBotHandler extends AbstractSupportHandler {
-   public void handle(String issue) {
-     
-   }
+    @Override
+    public void handle(String issue) {
+        if (issue.equals("password_reset")) {
+            System.out.println("[FAQBot] Handled password_reset");
+        } else if (next != null) {
+            System.out.println("[FAQBot] Cannot handle, escalating...");
+            next.handle(issue);
+        }
+    }
 }
